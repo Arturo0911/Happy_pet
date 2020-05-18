634b703f1956e3e2acdef9c5edae6c81a48da9e3
Arturo Negreiros <anegreiross@ooutlook.com>
2020-05-17 19:30:34 -0500

    add forms to generate dates

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,13 +1,53 @@
 const express = require('express');
 const router = express.Router();
 const { ItsLoggedIn, ItsNotLoggedIn } = require('../controllers/module');
+const pool = require('../connections/database');
+const path = require('path');
+const fs = require('fs-extra');
+const { createRandomNumber } = require('../connections/password')
 
 router.get('/', ItsLoggedIn, (req, res) => {
     res.render('routes/index');
 });
 
-router.get('/state', ItsLoggedIn, (req, res) => {
-    res.render('routes/state');
+router.get('/logger', (req, res) => {
+    res.render('routes/logger');
+});
+
+router.post('/logger', async(req, res) => {
+
+    const file_name = createRandomNumber();
+    const old_path = req.file.path;
+    const extname = path.extname(req.file.originalname).toLocaleLowerCase(); //extension name from file
+    const new_path = path.resolve(`src/public/img/img_pet/${file_name}${extname}`); // destiny we will be save images
+
+    const data_insert = {
+        ced: req.body.ced,
+        fullname: req.body.fullname,
+        pet_name: req.body.pet_name,
+        email: req.body.email,
+        image: file_name + extname,
+        address: req.body.address,
+        phone: req.body.phone
+    };
+
+    if (extname === '.png' || extname === '.jpg' || extname === '.jpeg' || extname === '.gif') {
+        fs.rename(old_path, new_path);
+    }
+    await pool.query('INSERT INTO clients SET?', [data_insert]);
+    req.flash('success', 'Cita agendad correctamente');
+    res.redirect('/main/state');
+
+});
+
+
+
+/*
+ first, we need to create form to clients can loger their information about pet
+*/
+router.get('/state', ItsLoggedIn, async(req, res) => {
+    const datas = await pool.query('SELECT * FROM clients');
+    res.render('routes/state', { resultado: datas });
 });
 
 
