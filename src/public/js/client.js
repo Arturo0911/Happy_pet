3b76be506543e94ca8e9f16f55c948fe0089b612
Arturo Negreiros <anegreiross@ooutlook.com>
2020-05-24 03:23:32 -0500

    updated at 24/05/2020

--- src/public/js/client.js
+++ src/public/js/client.js
@@ -3,11 +3,44 @@ const co_main_sidebar = document.getElementById('sidebar_content');
 const button = document.getElementById('Button_click');
 
 
-
-
+const select = document.getElementById('inputTypeFood');
+const select_pet_range = document.getElementById('InputRangePet');
 
 button.addEventListener('click', function(event) {
     main_sidebar.classList.toggle('active');
     co_main_sidebar.classList.toggle('active');
     event.preventDefault();
+});
+
+const type_pet_humedo = ['Adultos, cena de pavo; trozos jugosos', 'Adultos, festival de pollo; trozos jugosos',
+    'Cachorros, Pollo; Trozos Jugosos', 'Adultos, picnic cordero'
+];
+
+const type_pet_seco = ['Adultos (madianos y grandes)', 'Adultos (minis y pequeños)',
+    'cachorros (medianos y grandes)', 'cachorros salud visible (minis y pequeños)',
+    'Adultos (minis y pequeños) general', 'Adultos 7+', 'Adultos control de peso, todos los tamaños'
+];
+
+select.addEventListener('change', () => {
+    const seleccionado = $('#inputTypeFood').val();
+    if (seleccionado === 'Humedo') {
+        console.log(seleccionado);
+        let html = '';
+        type_pet_humedo.forEach(Element => {
+            html += `
+                <option>${Element}</option>
+            `
+        });
+        $('#InputRangePet').html(html);
+
+    } else {
+        let html = '';
+        type_pet_seco.forEach(Element => {
+            html += `
+                <option>${Element}</option>
+            `
+        });
+        $('#InputRangePet').html(html);
+    }
+
 });
--- src/routes/stock.js
+++ src/routes/stock.js
@@ -1,21 +1,58 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../connections/database');
+const { createRandomNumber } = require('../connections/password');
 const path = require('path');
 const fs = require('fs-extra');
 
 router.get('/add', (req, res) => {
     res.render('stock/add');
 });
+router.post('/add', async(req, res) => {
 
+    /**
+     * files
+     */
+    const file_name = createRandomNumber();
+    const extension = path.extname(req.file.originalname).toLocaleLowerCase();
+    const old_path = req.file.path;
+    const new_path = path.resolve(`src/public/img/profile_stock/${file_name}${extension}`);
+    const body_to_insert = {
+        name: req.body.name,
+        manufacturer_name: req.body.manufacturer_name,
+        tipo_alimento: req.body.tipo_alimento,
+        rango_edad_mascota: req.body.rango_edad_mascota,
+        foto: file_name + extension,
+        descripcion: req.body.descripcion,
+        ingredientes_nutricion: req.body.ingredientes_nutricion,
+        proteina_cruda_min: req.body.proteina_cruda_min,
+        grasa_cruda_min: req.body.grasa_cruda_min,
+        fibra_cruda_max: req.body.fibra_cruda_max,
+        humedad_max: req.body.humedad_max,
+        cenizas_max: req.body.cenizas_max,
+        calcio_min: req.body.calcio_min,
+        calcio_max: req.body.calcio_max,
+        fosforo_min: req.body.fosforo_min,
+        fosforo_max: req.body.fosforo_max,
+        fecha_elaboracion: req.body.fecha_elaboracion,
+        fecha_caducidad: req.body.fecha_caducidad
+    };
+    if (extension === '.png' || extension === '.jpg' || extension === '.jpeg' || extension === '.gif') {
+        fs.rename(old_path, new_path);
+    }
+    await pool.query('INSERT INTO stock SET?', [body_to_insert]);
+    req.flash('success', 'se agregó correctamente el producto');
+    res.redirect('/stck/state');
+});
 
 router.get('/pack_off', (req, res) => {
     res.render('stock/pack_off');
 });
 
 
-router.get('/state', (req, res) => {
-    res.render('stock/state');
+router.get('/state', async(req, res) => {
+    const states = await pool.query('SELECT * FROM stock');
+    res.render('stock/state', { stocks: states });
 });
 
 
