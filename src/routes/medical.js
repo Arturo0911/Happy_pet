a299b6b6bd647a84764bf9d35de06f18e61ab286
Arturo Negreiros <anegreiross@ooutlook.com>
2020-05-30 02:14:51 -0500

    added nodemailer

--- src/routes/medical.js
+++ src/routes/medical.js
@@ -5,4 +5,12 @@ router.get('/index', (req, res) => {
     res.render('medical/index');
 });
 
+router.get('/new_historial', (req, res) => {
+    res.render('medical/historial');
+});
+router.post('/new_historial', (req, res) => {
+    console.log(req.body);
+    res.redirect('/medical/index');
+});
+
 module.exports = router;
